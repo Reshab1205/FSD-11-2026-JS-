236f90f1bdf65caf25bfb226faecdf364bd3dd45
Reshab Kumar <rkmodi12051998@gmail.com>
2026-03-11 19:07:11 +0530

    dom 1

--- Function.js
+++ Function.js
@@ -68,42 +68,120 @@
 //     console.log(num1 / num2)
 // }
 
-function totalAmt(amt, gst) {
-  const gstAmt = (amt * gst) / 100;
-  const totalAmt = amt + gstAmt;
-  return totalAmt;
-}
+// function totalAmt(amt, gst) {
+//   const gstAmt = (amt * gst) / 100;
+//   const totalAmt = amt + gstAmt;
+//   return totalAmt;
+// }
 
-// console.log(totalAmt(100, 18));
+// // console.log(totalAmt(100, 18));
+
+// function emiCalculator(p,r,t) {
+//     const intAmt = (p * r * t) / 100;
+//     const totalAmt = p + intAmt
+//     const month = t * 12;
+//     const emi = totalAmt / month
+//     console.log(emi)
+// }
+
+// emiCalculator(1000000, 10, 5)
+
+// const products = [
+//   {
+//     product_name: "Lays",
+//     product_price: 100,
+//     product_gst: 12,
+//   },
+//   {
+//     product_name: "Kurkure",
+//     product_price: 200,
+//     product_gst: 13,
+//   },
+//   {
+//     product_name: "Pasta",
+//     product_price: 500,
+//     product_gst: 18,
+//   },
+// ];
+
+// TotalAmount :-
+
+// Array.isArray
 
 
-function emiCalculator(p,r,t) {
-    const intAmt = (p * r * t) / 100;
-    const totalAmt = p + intAmt
-    const month = t * 12;
-    const emi = totalAmt / month
-    console.log(emi)
-}
 
-emiCalculator(1000000, 10, 5)
+// const arr = [1, 2, [3, 4, [5, 6, [7, 8]]]];
+// function flatArray(arr) {
+//   const res = [];
+//   for (i of arr) {
+//     if (Array.isArray(i)) {
+//       res.push(...flatArray(i));
+//     } else {
+//       res.push(i);
+//     }
+//   }
+//   return res
+// }
 
-const products = [
-  {
-    product_name: "Lays",
-    product_price: 100,
-    product_gst: 12,
-  },
-  {
-    product_name: "Kurkure",
-    product_price: 200,
-    product_gst: 13,
-  },
-  {
-    product_name: "Pasta",
-    product_price: 500,
-    product_gst: 18,
-  },
-];
+// console.log(flatArray(arr));
 
+// [1,2,3,4,5,6,7,8]
 
-// TotalAmount :- 
+// console.time()
+
+// let obj = {
+//   name: "Reshab",
+//   age: 27,
+//   address: {
+//     village: "Matiara Tok",
+//     po: "Sarai",
+//     city: "Vaishali",
+//     state: "Bihar",
+//   },
+//   hobby: "Teaching", // make a function which would fetch all keys( nested obj also) inside the obj
+//   skills: {
+//     frontend: {
+//       skill_1: "HTML",
+//       skill_2: "CSS",
+//       skill_3: "JS",
+//       skill_4: "React",
+//       skill_5: "NextJS",
+//       component: {
+//         project: "EMI calculator",
+//         summary: {
+//           emi: "Calc EMi",
+//           data: {
+//             hello: "Hello",
+//           },
+//         },
+//       },
+//     },
+//     backend: {
+//       skill_5: "NodeJs",
+//       skill_6: "Express",
+//       skill_7: "NestJS",
+//       skills: {
+//         hello: "Hii"
+//       }
+//     },
+//     db: "MongoDb",
+//   },
+// };
+
+// console.log(obj[0])
+// console.log(obj.hobby)
+
+// function fetchKeys(obj) {
+//   for (i in obj) {
+//     if (typeof obj[i] == "object"){
+//       // console.log(i)
+//       fetchKeys(obj[i]);
+//     } else {
+//       console.log(i)
+//     }
+//   }
+// }
+
+// fetchKeys(obj);
+
+// console.timeEnd()
