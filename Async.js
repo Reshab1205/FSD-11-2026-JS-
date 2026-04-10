d13d483c2da27d9d01559b3faaec6ba1c8a643d5
Reshab Kumar <rkmodi12051998@gmail.com>
2026-04-10 19:03:54 +0530

    callback

--- Async.js
+++ Async.js
@@ -89,24 +89,24 @@
 
 // Call apply bind
 
-const obj = {
-  fname: "Reshab",
-  age: 27,
-  greet: function (city, city1) {
-    console.log("My Name is " + this.fname + " my age is " + this.age + " i am from " + city + city1);
-  },
-};
-const user = { fname: "Ravi", age: 28 };
-const result = obj.greet.bind(user, ["Bhopal"])
-console.log(typeof result)
-result()
+// const obj = {
+//   fname: "Reshab",
+//   age: 27,
+//   greet: function (city, city1) {
+//     console.log("My Name is " + this.fname + " my age is " + this.age + " i am from " + city + city1);
+//   },
+// };
+// const user = { fname: "Ravi", age: 28 };
+// const result = obj.greet.bind(user, ["Bhopal"])
+// console.log(typeof result)
+// result()
 
-const user1 = { fname: "Baibhav" };
-const users = [
-  { fname: "Ravi", age: 28, city: "Delhi" },
-  { fname: "Baibhav", age: 27, city: "HYD" },
-  { fname: "Avinash", age: 28, city: "BEN" },
-];
+// const user1 = { fname: "Baibhav" };
+// const users = [
+//   { fname: "Ravi", age: 28, city: "Delhi" },
+//   { fname: "Baibhav", age: 27, city: "HYD" },
+//   { fname: "Avinash", age: 28, city: "BEN" },
+// ];
 
 // for(let i of users) {
 //   // console.log(i)
@@ -114,8 +114,69 @@ const users = [
 // }
 // obj.greet.call(user);
 
-const app = function () {
-  const data = 'Hello'
-  return data
+// const app = function () {
+//   const data = 'Hello'
+//   return data
+// }
+// console.log(app())
+// console.time()
+//Order of execution
+// function hello() {
+//   console.log('Hello')
+// }
+// setTimeout(() => {
+//   console.log('Ist')
+// })
+
+// setInterval(() => {
+//   hello()
+// })
+// // console.timeEnd()
+// console.log('Hello');
+// console.log('Hii');
+// Promise.resolve().then(() => console.log('Promise'))
+
+
+
+function greet(callback) {
+  console.log('Hello')
+  callback()
 }
-console.log(app())
+
+function fname() {
+  console.log('Reshab')
+}
+
+function fname1() {
+  console.log('Ravi')
+}
+
+greet(fname)
+greet(fname1)
+
+
+const arr = ["Reshab", "Ravi", "Baibhav", "Avinash", "Shubham"];
+for(let i of arr) {
+  sendSmsService(i, sendDiwaliGreeting)
+}
+
+//Sending Msg to Reshab
+// Happy New Year Reshab
+
+
+function sendSmsService(name, callback) {
+  console.log(`Sending MSg to ${name}`)
+  callback(name)
+}
+
+function sendEmailService() {
+
+}
+
+function sendNewYearGreeting(name) {
+  console.log(`Happy New Year ${name}`)
+}
+
+function sendDiwaliGreeting(name) {
+  console.log(`Happy Diwali ${name}`)
+}
